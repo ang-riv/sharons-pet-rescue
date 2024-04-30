87546048ec45a17252cd0f159ac216d264e3b25f
a.rivera <a.riveraa99@gmail.com>
2024-04-29 21:28:39 -0500

    Added showPets function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //const statusButton = document.querySelector("button");
-//const pets = document.querySelector(".all-pets");
+const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species){
     const pet = {
@@ -39,4 +39,33 @@ clover.sleep();
 baxter.play();
 
 // checking isTired meters
-console.log(clover, baxter);
+//console.log(clover, baxter);
+
+// display pet objects challenge
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+// create an array of objects
+const allPets = [sora, clover, baxter, cleo, francine];
+
+// lot out the array
+//console.log(allPets);
+
+const showPets = function(petArray) {
+    // clears the list whenever this function is run 
+    pets.innerHTML = "";
+    for(animal of petArray){
+        // default status, the pet can play
+        let status = "ready to play!";
+        if(animal.isTired >= 7){
+            // if tiredness is high, the pet is sleeping
+            status = "sleeping.";
+        }
+        // create a list element
+        let li = document.createElement("li");
+        li.innerHTML = `<span class = "pet-name">${animal.name}</span>, the ${animal.species}, is ${status}`
+        pets.append(li);
+    }
+};
+
