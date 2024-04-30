d75467dcf17ca10f6bb692cad6c1d59703daa384
a.rivera <a.riveraa99@gmail.com>
2024-04-29 21:16:08 -0500

    Verify that methods work and tested isTired meters

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const statusButton = document.querySelector("button");
-const pets = document.querySelector(".all-pets");
+//const statusButton = document.querySelector("button");
+//const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species){
     const pet = {
@@ -9,7 +9,7 @@ const createPet = function (name, species){
         sleep: function (){
             console.log(`${this.name} needs a nap. Zzz...`);
             // after sleeping, resets the tired meter down to zero
-            isTired = 1;
+            this.isTired = 1;
         },
         play: function(){
             if (this.isTired === 10) {
@@ -18,7 +18,7 @@ const createPet = function (name, species){
             } else {
                 console.log(`Yay! ${this.name} loves to play!`)
                 // add to it because playing makes pets tired
-                isTired += 1;
+                this.isTired += 1;
             }
         }
     };
@@ -31,3 +31,12 @@ const clover = createPet("Clover", "rabbit");
 const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
+
+// verify the objects and methods
+// console.log(sora, clover, baxter, cleo, francine);
+// checking if methods work
+clover.sleep();
+baxter.play();
+
+// checking isTired meters
+console.log(clover, baxter);
