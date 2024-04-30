bbaa7633c8e648e030a6147c97a2a2b02eb7d477
a.rivera <a.riveraa99@gmail.com>
2024-04-29 21:09:16 -0500

    Created factory function with methods

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species){
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5,
+        sleep: function (){
+            console.log(`${this.name} needs a nap. Zzz...`);
+            // after sleeping, resets the tired meter down to zero
+            isTired = 1;
+        },
+        play: function(){
+            if (this.isTired === 10) {
+                console.log("Too tired to play.");
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`)
+                // add to it because playing makes pets tired
+                isTired += 1;
+            }
+        }
+    };
+
+    return pet;
+}
